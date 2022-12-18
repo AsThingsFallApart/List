5646bd6306af9b97126ecbdd76156722d88b5b69
ATFA <AsThingsFallApart@gmail.com>
2022-12-17 22:56:03 -0500

    'create row' button creates row

--- behavior.js
+++ behavior.js
@@ -3,5 +3,20 @@ let rowCreator = document.getElementById('creator');
 rowCreator.addEventListener('click', createRow);
 
 function createRow() {
-  alert('test');
+  let container = document.getElementById('tableList');
+  let inputText = document.getElementById('inputBox').value;
+
+  let listRowIndicator = document.createElement('td');
+  listRowIndicator.classList.add('tableListRowIndicator');
+
+  let listRowDescriptor = document.createElement('td');
+  listRowDescriptor.classList.add('tableListRowDescriptor');
+  listRowDescriptor.textContent = inputText;
+
+  let newListRow = document.createElement('tr');
+  newListRow.classList.add('tableListRow');
+  newListRow.appendChild(listRowIndicator);
+  newListRow.appendChild(listRowDescriptor);
+
+  container.appendChild(newListRow);
 }
