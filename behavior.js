81524d68528e8c6cd6e1a375becd1b76f677cb1d
ATFA <AsThingsFallApart@gmail.com>
2022-12-19 14:06:17 -0500

    change color of indicator on click

--- behavior.js
+++ behavior.js
@@ -1,13 +1,37 @@
 let rowCreator = document.getElementById('creator');
-
 rowCreator.addEventListener('click', createRow);
 
+let rowIndicatorsWaitlist = document.getElementsByClassName(
+  'tableListRowIndicatorWaitlist'
+);
+for (let i = 0; i < rowIndicatorsWaitlist.length; i++) {
+  rowIndicatorsWaitlist[i].addEventListener('click', handleIndicatorClick);
+}
+
+let rowIndicatorsOpen = document.getElementsByClassName(
+  'tableListRowIndicatorOpen'
+);
+for (let i = 0; i < rowIndicatorsOpen.length; i++) {
+  rowIndicatorsOpen[i].addEventListener('click', handleIndicatorClick);
+}
+
+console.log(rowIndicatorsWaitlist);
+console.log(rowIndicatorsOpen);
+
+function handleIndicatorClick(event) {
+  if (event.target.classList == 'tableListRowIndicatorWaitlist') {
+    event.target.classList = 'tableListRowIndicatorOpen';
+  } else if (event.target.classList == 'tableListRowIndicatorOpen') {
+    event.target.classList = 'tableListRowIndicatorWaitlist';
+  }
+}
+
 function createRow() {
-  let container = document.getElementById('tableList');
+  let list = document.getElementById('tableList');
   let inputText = document.getElementById('inputBox').value;
 
   let listRowIndicator = document.createElement('td');
-  listRowIndicator.classList.add('tableListRowIndicator');
+  listRowIndicator.classList.add('tableListRowIndicatorWaitlist');
 
   let listRowDescriptor = document.createElement('td');
   listRowDescriptor.classList.add('tableListRowDescriptor');
@@ -18,5 +42,5 @@ function createRow() {
   newListRow.appendChild(listRowIndicator);
   newListRow.appendChild(listRowDescriptor);
 
-  container.appendChild(newListRow);
+  list.appendChild(newListRow);
 }
